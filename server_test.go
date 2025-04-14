5b2c192f19cf3abdc5b9daf373f1002026a6e2d1
Kenneth Kam <kenkam@gmail.com>
2025-04-14 18:08:45 +0100

    Simplify tests

--- server_test.go
+++ server_test.go
@@ -13,46 +13,22 @@ func TestServerSupportsGzip(t *testing.T) {
 	log.SetOutput(io.Discard)
 
 	s := NewServer("localhost", 7777, "./testdata")
-	errCh := make(chan error)
-	getCh := make(chan error)
-	doneCh := make(chan bool)
-	go func() {
-		err := s.Listen()
-		errCh <- err
-	}()
+	go s.Listen()
 
-	go func() {
-		<-s.ListenCh
+	<-s.ListenCh
 
-		req, _ := http.NewRequest("GET", "http://localhost:7777/index.html", nil)
-		req.Header.Add("Accept-Encoding", "gzip")
-		r, err := http.DefaultClient.Do(req)
-		if err != nil {
-			getCh <- err
-			return
-		}
+	req, _ := http.NewRequest("GET", "http://localhost:7777/index.html", nil)
+	req.Header.Add("Accept-Encoding", "gzip")
+	r, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Error(err)
+	}
 
-		b, _ := os.ReadFile("./testdata/index.html")
-		if r.ContentLength >= int64(len(b)) {
-			t.Error("compressed response should be less than the original length")
-		}
-		if r.Header["Content-Encoding"][0] != "gzip" {
-			t.Error("returned response does not have Content-Encoding: gzip")
-		}
-
-		doneCh <- true
-	}()
-
-	for {
-		select {
-		case err := <-errCh:
-			t.Error(err)
-			return
-		case err := <-getCh:
-			t.Error(err)
-			return
-		case <-doneCh:
-			return
-		}
+	b, _ := os.ReadFile("./testdata/index.html")
+	if r.ContentLength >= int64(len(b)) {
+		t.Error("compressed response should be less than the original length")
+	}
+	if r.Header["Content-Encoding"][0] != "gzip" {
+		t.Error("returned response does not have Content-Encoding: gzip")
 	}
 }
