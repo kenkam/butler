24b7acbaf106fff9d806f4e4e40495168b782af7
Kenneth Kam <kenkam@gmail.com>
2025-04-17 18:24:05 +0100

    Close response body

--- handlers.go
+++ handlers.go
@@ -56,6 +56,7 @@ func (b backendHandler) Handle(c *Context) (bool, error) {
 			c.Response = BadGateway()
 			return true, nil
 		}
+		defer resp.Body.Close()
 
 		b, err := io.ReadAll(resp.Body)
 		if err != nil {
